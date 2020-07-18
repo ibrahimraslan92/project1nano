7f47dde97b9d728cd9cfe157c8bdacf01434fc83
ibrahimraslan92 <63014665+ibrahimraslan92@users.noreply.github.com>
2020-07-18 05:44:34 +0200

    Add files via upload

--- project1.js
+++ project1.js
@@ -13,7 +13,11 @@ let sections = document.getElementsByTagName("section");
  * Start Helper Functions
  * 
 */
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -83,15 +87,10 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
